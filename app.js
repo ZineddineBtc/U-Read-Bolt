c865d821dda43c9e302aaa668515683f5cd3b8bc
Zineddine Bettouche <zineddine.bettouche.inbox@gmail.com>
2021-02-25 16:40:27 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+//jshint esversion:6
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.static("public"));
