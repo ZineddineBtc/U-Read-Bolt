1323ae6ae301d4d4007f1ef9d1aaabb1895242ee
Zineddine Bettouche <zineddine.bettouche.inbox@gmail.com>
2021-03-03 18:12:32 +0100

    index.ejs renamed to upload.ejs    &    index.ejs created and implemented

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const fs = require("fs");
 const PDFParser = require("./node_modules/pdf2json/PDFParser");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
 app.use(express.static("public"));
@@ -14,9 +19,56 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(fileUpload());
 app.set("view engine", "ejs");
 
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialize: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+mongoose.set("useCreateIndex", true);
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+    googleId: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = mongoose.model("user", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(function(user, done) {done(null, user.id);});
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {done(err, user);});
+});
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    passReqToCallback: true,
+    userProfileURL: "https://googleapis.com/oauth2/v3/userinfo"
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
+
 app.get("/", function(req, res){
     res.render("index"); 
 });
+
+app.get("/upload", function(req, res){
+    res.render("upload"); 
+});
+
 
 app.get("/game", function(req, res){
     res.render("game");
@@ -69,7 +121,7 @@ app.post("/text", function(req, res){
 
 app.get("/read", function(req, res){
     if(text === "empty"){
-        res.redirect("/");
+        res.redirect("/upload");
     } else {
         res.render("viewer", {text: text});
     }
