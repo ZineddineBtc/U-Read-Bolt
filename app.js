299e9da5609a734aad13d05bffda123183344c0a
Zineddine Bettouche <zineddine.bettouche.inbox@gmail.com>
2021-03-07 14:18:23 +0100

    testing changes on heroku

--- app.js
+++ app.js
@@ -153,11 +153,13 @@ app.post("/upload", function(req, res) {
       return res.status(400).send("No files were uploaded.");
     }
     const uploadedFile = req.files.uploadedFile;
-    const uploadPath = __dirname + "/public/uploads/" + uploadedFile.name;
-  
+    var uploadPath = __dirname + "/public/uploads/" + uploadedFile.name;
+    uploadPath.replace(" ", "-");
     // Use the mv() method to place the file somewhere on server
     uploadedFile.mv(uploadPath, function(error) {
-        if (error) return res.status(500).send(error);
+        if (error){
+            return res.status(500).send("ERROR");
+        } 
         const textPath = __dirname + "/public/F1040EZ.txt"
         let pdfParser = new PDFParser(this, 1); 
         pdfParser.on("pdfParser_dataError", errorData => console.error(errorData.parserError));
