7c995cc898d35173c10dc3b8b18821de0f0b570f
Zineddine Bettouche <zineddine.bettouche.inbox@gmail.com>
2021-03-08 23:05:42 +0100

    PDF file size limited. jquery of toggling dynamics adjusted

--- public/javascript/upload.js
+++ public/javascript/upload.js
@@ -1,17 +1,17 @@
-$("#input-upload").height($("#textarea-index").height());
+$("#input-upload").height($("#textarea-upload").height());
 
 let upload = true;
 $("#h1-text").click(function(){
     if(upload){
         upload = false;
         $(this).css("color", "#000");
-        $("#textarea-index").attr("disabled", false);
-        $("#button-read").attr("disabled", false);
-        $("#textarea-index").css("background-color", "#212529");
+        $("#textarea-upload").attr("disabled", false);
+        checkTextArea();
+        $("#textarea-upload").css("background-color", "#212529");
         $("#h1-upload").css("color", "#6f7275");
         $("#input-upload").attr("disabled", true);
         $("#input-upload").css("background-color", "#6f7275");
-        checkSelectedFile();
+        $("#button-upload").attr("disabled", true);
     }
 });
 
@@ -20,32 +20,45 @@ $("#h1-upload").click(function(){
         upload = true;
         $(this).css("color", "#000");
         $("#input-upload").attr("disabled", false);
-        $("#button-upload").attr("disabled", false);
+        checkSelectedFile();
         $("#input-upload").css("background-color", "#212529");
         $("#h1-text").css("color", "#6f7275");
-        $("#textarea-index").attr("disabled", true);
-        $("#textarea-index").css("background-color", "#6f7275");
-        checkTextArea();
+        $("#textarea-upload").attr("disabled", true);
+        $("#textarea-upload").css("background-color", "#6f7275");
+        $("#button-text").attr("disabled", true);
     }
 });
 
 $("#input-upload").change(checkSelectedFile);
 function checkSelectedFile() {
+    if(!$("#input-upload").val()) {
+        alert("Please select a PDF file");
+        $("#button-upload").attr("disabled", true);
+        return;
+    }
     const fileName = $(this).val();
     const extension = fileName.split(".")[fileName.split(".").length-1];
     console.log(extension);
     if (extension === "pdf") {
-        $("#button-upload").attr("disabled", false);
+        const fileSize = this.files[0].size/1024/1024;
+        if(fileSize < 30) {
+            $("#button-upload").attr("disabled", false);
+        } else {
+            alert("Size should not exceed 30 MiB");
+            $("#input-upload").val("");
+            $("#button-upload").attr("disabled", true);
+        }
     } else {
         alert("Please upload a PDF file exclusively");
-        $("#input-index").val(null);
+        $("#input-upload").val("");
         $("#button-upload").attr("disabled", true);
     }
+
 }
 
-$("#textarea-index").bind("input propertychange", checkTextArea);
+$("#textarea-upload").bind("input propertychange", checkTextArea);
 function checkTextArea() {
-    if($(this).val().length>0) {
+    if($("#textarea-upload").val()) {
         $("#button-read").attr("disabled", false);
     } else {
         $("#button-read").attr("disabled", true);
