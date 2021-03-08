4a69553958716472248147a29a42072785d529a1
Zineddine Bettouche <zineddine.bettouche.inbox@gmail.com>
2021-03-08 13:13:10 +0100

    reading types implemented in viewer.js. Unresolved problem when reaching the end of the text

--- public/javascript/viewer.js
+++ public/javascript/viewer.js
@@ -1,4 +1,5 @@
 
+///////////////////////////// Dark Mode //////////////////////////////////
 $("#checkbox-dark-mode").change(function() {
     if (this.checked) {
         $(".bgc-dark-mode").css("background-color", "#212529");
@@ -20,6 +21,8 @@ $("#checkbox-dark-mode").change(function() {
         $(".h1-dark-mode").css("visibility", "hidden");
     }
 });
+
+///////////////////////////// Speed Adjustment //////////////////////////
 let inputSpeed = $("#input-speed").val();
 $("#span-btn-go-back-words").html(" "+inputSpeed +" Words Back");
 let speed = 1000 / inputSpeed;
@@ -42,15 +45,20 @@ $("#btn-increase-speed").click({action: "increase"}, adjustSpeed);
 $("#btn-decrease-speed").click({action: "decrease"}, adjustSpeed);
 $("#input-speed").on("input", {action: "adjust"}, adjustSpeed);
 
+///////////////////////////// Reading Functions /////////////////////////
 let text = $("#p-viewer").text().replace(/(\r\n|\n|\r)/gm, "");
 let pageArray = text.split(/(----------------Page \(\d\) Break----------------)/);
-let index = 0, pageIndex = 0, wordInterval, word;
+let index = 0, sentenceIndex = 0, sentenceWordIndex = 0,
+    pageIndex = 0, wordInterval, word;
+let lastChecked = "word";
 function byWord() {
     if(!running) return;
+    if(lastChecked !== "word") index = 0;
+    lastChecked = "word";
     wordInterval = window.setInterval(function(){
         wordsArray = pageArray[pageIndex].split(" ");
         word = wordsArray[index];
-        $("#h1-viewer").text(word);
+        $("#span-middle").text(word);
         if(word[word.length-1] == "."){
             clearInterval(wordInterval);
             setTimeout(byWord, 2*speed);
@@ -79,7 +87,120 @@ function byWord() {
         }
     }, speed);
 }
+function byLights() {
+    if(!running) return;
+    if(lastChecked !== "lights"){ 
+        sentenceIndex = 0; 
+        sentenceWordIndex = 0;
+        $("#span-middle").text(" ");
+    }
+    lastChecked = "lights";
+    wordInterval = window.setInterval(function(){
+        var page = pageArray[pageIndex];
+        var sentencesArray = page.split(".");
+        var sentence = sentencesArray[sentenceIndex];
+        var wordsArray = sentence.split(" ");
+        wordsArray[wordsArray.length-1] += ".";
+        
+        var b="", m="", a="";
+        for(var j=0; j<sentenceWordIndex; j++){
+            b += wordsArray[j] + " ";
+        }
+        m = wordsArray[sentenceWordIndex];
+        for(var k=sentenceWordIndex+1; k<wordsArray.length; k++){
+            a += wordsArray[k] + " ";
+        }
+        $("#span-before").text(b);
+        $("#span-middle").text(m);
+        $("#span-after").text(a);
+        
+        if(sentenceWordIndex < wordsArray.length-1){
+            sentenceWordIndex++;
+        } else {
+            sentenceWordIndex = 0;
+            if(sentenceIndex < sentencesArray.length-1){
+                sentenceIndex++;
+            } else {
+                sentenceIndex = 0;
+                const totalPages = (pageArray.length/2)-1;
+                let p = $("#input-page").val();
+                if(pageIndex < totalPages){
+                    pageIndex += 2;
+                    p++;
+                } else {
+                    pageIndex = 0;
+                    p = 1;
+                    $("#input-page").val(p);
+                    clearInterval(wordInterval);
+                    setTimeout(byLights, 3*speed);
+                }
+                $("#input-page").val(p);
+                clearInterval(wordInterval);
+                setTimeout(byLights, 2*speed);
+            }
+        }
+        clearInterval(wordInterval);
+        setTimeout(byLights, speed);
+    }, speed);
+}
+function byReviel() {
+    if(!running) return;
+    if(lastChecked !== "reviel"){ 
+        sentenceIndex = 0; 
+        sentenceWordIndex = 0;
+        $("#span-middle").text(" ");
+    }
+    lastChecked = "reviel";
+    var m = "";
+    wordInterval = window.setInterval(function(){
+        var page = pageArray[pageIndex];
+        var sentencesArray = page.split(".");
+        var sentence = sentencesArray[sentenceIndex];
+        var wordsArray = sentence.split(" ");
+        wordsArray[wordsArray.length-1] += ".";
 
+        console.log("pageIndex: "+ pageIndex);
+        console.log("sentenceIndex: "+ sentenceIndex);
+        console.log("sentenceWordIndex: " + sentenceWordIndex);
+
+        m += wordsArray[sentenceWordIndex] + " ";
+        console.log("m: "+m);
+
+        $("#span-before").text("");
+        $("#span-middle").text(m);
+        $("#span-after").text("");
+        
+        if(sentenceWordIndex < wordsArray.length-1){
+            sentenceWordIndex++;
+        } else {
+            clearInterval(wordInterval);
+            sentenceWordIndex = 0;
+            m = "";
+            if(sentenceIndex < sentencesArray.length-1){
+                sentenceIndex++;
+                clearInterval(wordInterval);
+                setTimeout(byReviel, 2*speed);
+            } else {
+                sentenceIndex = 0;
+                const totalPages = (pageArray.length/2)-1;
+                let p = $("#input-page").val();
+                if(pageIndex < totalPages){
+                    pageIndex += 2;
+                    p++;
+                } else {
+                    pageIndex = 0;
+                    p = 1;
+                    $("#input-page").val(p);
+                    clearInterval(wordInterval);
+                }
+                $("#input-page").val(p);
+                setTimeout(byReviel, 2*speed);
+            }
+        }
+    }, speed);
+}
+
+/////////////////////////////// Run | Stop /////////////////////////////////
 let running = false;
 const inputIDs = ["btn-decrease-speed", "btn-increase-speed", "input-speed", "input-page", "btn-word-viewer", "btn-lights-viewer", "btn-reviel-viewer"];
 function disableInputs(disabled){
@@ -104,10 +225,20 @@ function startStop() {
         running = !running;
         $("#btn-start-viewer").removeClass(darkOrLightClass).addClass("btn-danger").html("STOP");
         disableInputs(true);
+        playCheckedType();
+    }
+}
+function playCheckedType(){
+    if($('#btn-word-viewer').is(':checked')) {
         byWord();
+    } else if($('#btn-lights-viewer').is(':checked')) {
+        byLights();
+    } else if($('#btn-reviel-viewer').is(':checked')) {
+        byReviel();
     }
 }
 
+///////////////////////////// Go-back Functions //////////////////////////////
 $("#btn-go-back-words").click(function() {
     if(running) startStop();
     index -= inputSpeed;
